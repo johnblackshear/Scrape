8cc6db1d35dcb720ef694ad3e1a5a13cd0c02546
John Blackshear <swjaybee@gmail.com>
2019-08-19 23:23:44 -0500

    getting routes

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 var express = require("express");
 
+var expressHandlebars = require("express-handlebars");
+
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+var router = express.Router();
+
+require("./config/routes")(router);
+
+
+app.use(express.static(__dirname + "/pulbic"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main",
+    
+}));
+
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
 var mongoose = require("mongoose");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -12,30 +41,6 @@ mongoose.connect(MONGODB_URI, function(error){
         console.log("mongoos connenction is successful");
     }
 });
-
-var expressHandlebars = require("express-handlebars");
-
-var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-
-var router = express.Router();
-
-app.use(express.static(__dirname + "/pulbic"));
-
-app.use(router);
-
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main"
-}));
-
-app.set("veiw engine", "handlebars");
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
 
 app.listen(PORT, function(){
     console.log("Listening on Port"  + PORT);
